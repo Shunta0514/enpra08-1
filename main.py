348986b05837728e8e52c18af9829f4e1060b4f2
SHUNTAs <kingdomshin16@gmail.com>
2022-06-09 22:22:23 +0900

    write exception handling in main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import sys
 from numeron import NumerOn
 
 if __name__ == '__main__':
     numeron = NumerOn()
     while(True):
-        answer = int( input("数字の被らない3桁の整数を入力してください\n"))
+        try:
+            answer = int( input("数字の被らない3桁の整数を入力してください\n"))
+        except ValueError:
+            print ("\033[31mError: \033[m数字以外を入力しないでください")
+            continue
+        if answer < 99 or 1000 <answer:
+            print("\033[31mError: \033[m3桁の数字を入力してください")
+            continue
+            
         Eat,  Bite = numeron.judge_answer(answer)
         print("Eat:",Eat)
         print("Bite",Bite)
